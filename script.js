2b90db6fb8f60160c105a4354c1c581df486c338
esixce <98708432+esixce@users.noreply.github.com>
2023-03-31 08:54:24 -0500

    url setup test

--- script.js
+++ script.js
@@ -1,172 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   (function () {
-    // Your code goes here
-
-    console.log("here");
-
-    // https://plotly.com/javascript
-    // https://plotly.com/javascript/range-slider/
-
-    // UNCOMMENT FOR WEB
-    // fetch('https://github.com/esixce/textDigest/blob/main/output/stemmed_tokens.csv')
-    // .then(response => response.text())
-    // .then(data => {
-    //   // Use Papa Parse to parse the CSV data into an array of objects
-    //   const parsedData = Papa.parse(data, { header: true }).data;
-
-    //   // Do something with the parsed data, such as printing it to the console
-    //   console.log(parsedData);
-    // })
-    // .catch(error => {
-    //   // Handle any errors that occur during the fetch request
-    //   console.error(error);
-    // });
-
-    // // // COMMENT OUT FOR WEB
-    // fetch("/input/stemmed_tokens.csv")
-    //   .then((response) => response.text())
-    //   .then((data) => {
-    //     // Use the data to create an NLTK visualization
-    //     //   console.log(data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching data:", error);
-    //   });
-
-
     const freqTable = {};
 
-    Papa.parse("/input/txtSpecs.csv", {
-      download: true,
-      header: true,
-      complete: function (results) {
-        results.data.forEach(function (row) {
-          freqTable[row.Token] = parseInt(row.Frequency);
-        });
-        wordCloud(freqTable)
-      },
-    });
+    baseUrl = "https://github.com/esixce/textDigest/blob/main/output/";
 
-    // // COMMENT OUT FOR WEB
-    fetch("/input/txtSpecs.csv")
+    // UNCOMMENT FOR WEB
+    fetch(baseUrl + "stemmed_tokens.csv")
       .then((response) => response.text())
-      .then((freqTable) => {
-        // Use the data to create an NLTK visualization
-        //   console.log(freqTable);
+      .then((data) => {
+        // Use Papa Parse to parse the CSV data into an array of objects
+        const freqTable = Papa.parse(data, { header: true }).data;
+
+        // Do something with the parsed data, such as printing it to the console
+        console.log(freqTable);
+        wordCloud(freqTable);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        // Handle any errors that occur during the fetch request
+        console.error(error);
       });
 
-    var trace1 = {
-      x: [1, 2, 3],
-      y: [4, 3, 2],
-      type: "scatter",
-    };
+    // Papa.parse("/input/txtSpecs.csv", {
+    //   download: true,
+    //   header: true,
+    //   complete: function (results) {
+    //     results.data.forEach(function (row) {
+    //       freqTable[row.Token] = parseInt(row.Frequency);
+    //     });
+    //     wordCloud(freqTable)
+    //   },
+    // });
 
-    var trace2 = {
-      x: [20, 30, 40],
-      y: [30, 40, 50],
-      xaxis: "x2",
-      yaxis: "y2",
-      type: "scatter",
-    };
-
-    var stuff = [trace1, trace2];
-
-    var layout3 = {
-      yaxis2: {
-        domain: [0.6, 0.95],
-        anchor: "x2",
-      },
-      xaxis2: {
-        domain: [0.6, 0.95],
-        anchor: "y2",
-      },
-    };
-
-    Plotly.newPlot("myDiv", stuff, layout3);
-
-    var rawDataURL =
-      "https://raw.githubusercontent.com/plotly/datasets/master/2016-weather-data-seattle.csv";
-    var xField = "Date";
-    var yField = "Mean_TemperatureC";
-
-    var selectorOptions = {
-      buttons: [
-        {
-          step: "month",
-          stepmode: "backward",
-          count: 1,
-          label: "1m",
-        },
-        {
-          step: "month",
-          stepmode: "backward",
-          count: 6,
-          label: "6m",
-        },
-        {
-          step: "year",
-          stepmode: "todate",
-          count: 1,
-          label: "YTD",
-        },
-        {
-          step: "year",
-          stepmode: "backward",
-          count: 1,
-          label: "1y",
-        },
-        {
-          step: "all",
-        },
-      ],
-    };
-
-    Plotly.d3.csv(rawDataURL, function (err, rawData) {
-      if (err) throw err;
-
-      var data = prepData(rawData);
-      var layout2 = {
-        title: "Time series with range slider and selectors",
-        xaxis: {
-          rangeselector: selectorOptions,
-          rangeslider: {},
-        },
-        yaxis: {
-          fixedrange: true,
-        },
-      };
-
-      Plotly.plot("graph", data, layout2, { showSendToCloud: true });
-    });
-
-    function prepData(rawData) {
-      var x = [];
-      var y = [];
-
-      console.log(rawData.length);
-
-      rawData.forEach(function (datum, i) {
-        if (i % 100) return;
-
-        x.push(new Date(datum[xField]));
-        y.push(datum[yField]);
-      });
-
-      return [
-        {
-          mode: "lines",
-          x: x,
-          y: y,
-        },
-      ];
-    }
 
 
     function wordCloud(freqTable) {
-
       const otherThing = {
         apple: 10,
         banana: 8,
@@ -219,3 +86,145 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   })();
 });
+
+// // // COMMENT OUT FOR WEB
+// fetch("/input/stemmed_tokens.csv")
+//   .then((response) => response.text())
+//   .then((data) => {
+//     // Use the data to create an NLTK visualization
+//     //   console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
+
+// https://plotly.com/javascript
+// https://plotly.com/javascript/range-slider/
+
+// // // COMMENT OUT FOR WEB
+// fetch("/input/txtSpecs.csv")
+//   .then((response) => response.text())
+//   .then((freqTable) => {
+//     // Use the data to create an NLTK visualization
+//     //   console.log(freqTable);
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
+
+
+
+
+
+// var trace1 = {
+//     x: [1, 2, 3],
+//     y: [4, 3, 2],
+//     type: "scatter",
+//   };
+
+//   var trace2 = {
+//     x: [20, 30, 40],
+//     y: [30, 40, 50],
+//     xaxis: "x2",
+//     yaxis: "y2",
+//     type: "scatter",
+//   };
+
+//   var stuff = [trace1, trace2];
+
+//   var layout3 = {
+//     yaxis2: {
+//       domain: [0.6, 0.95],
+//       anchor: "x2",
+//     },
+//     xaxis2: {
+//       domain: [0.6, 0.95],
+//       anchor: "y2",
+//     },
+//   };
+
+//   Plotly.newPlot("myDiv", stuff, layout3);
+
+//   var rawDataURL =
+//     "https://raw.githubusercontent.com/plotly/datasets/master/2016-weather-data-seattle.csv";
+//   var xField = "Date";
+//   var yField = "Mean_TemperatureC";
+
+//   var selectorOptions = {
+//     buttons: [
+//       {
+//         step: "month",
+//         stepmode: "backward",
+//         count: 1,
+//         label: "1m",
+//       },
+//       {
+//         step: "month",
+//         stepmode: "backward",
+//         count: 6,
+//         label: "6m",
+//       },
+//       {
+//         step: "year",
+//         stepmode: "todate",
+//         count: 1,
+//         label: "YTD",
+//       },
+//       {
+//         step: "year",
+//         stepmode: "backward",
+//         count: 1,
+//         label: "1y",
+//       },
+//       {
+//         step: "all",
+//       },
+//     ],
+//   };
+
+//   Plotly.d3.csv(rawDataURL, function (err, rawData) {
+//     if (err) throw err;
+
+//     var data = prepData(rawData);
+//     var layout2 = {
+//       title: "Time series with range slider and selectors",
+//       xaxis: {
+//         rangeselector: selectorOptions,
+//         rangeslider: {},
+//       },
+//       yaxis: {
+//         fixedrange: true,
+//       },
+//     };
+
+//     Plotly.plot("graph", data, layout2, { showSendToCloud: true });
+//   });
+
+//   function prepData(rawData) {
+//     var x = [];
+//     var y = [];
+
+//     console.log(rawData.length);
+
+//     rawData.forEach(function (datum, i) {
+//       if (i % 100) return;
+
+//       x.push(new Date(datum[xField]));
+//       y.push(datum[yField]);
+//     });
+
+//     return [
+//       {
+//         mode: "lines",
+//         x: x,
+//         y: y,
+//       },
+//     ];
+//   }
+
+
+
+// Plotly.newPlot( tester, [{
+//     x: [1, 2, 3, 4, 5],
+//     y: [1, 2, 4, 8, 16] }], {
+//     margin: { t: 0 } } );
